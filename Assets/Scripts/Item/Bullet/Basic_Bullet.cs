c5fb6002e7e410ca076f64d8312b071d241e5f44
BigPong <bigpong88508@gmail.com>
2020-10-10 19:43:11 +0800

    10/10 Ice lance WIP

---
+++ Assets/Scripts/Item/Bullet/Basic_Bullet.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Basic_Bullet : MonoBehaviour
+{
+    [Header("Setting")]
+    public float DestroyAfterCollision;
+    public LayerMask TargetMask;
+
+    [Header("Physics")]
+    [SerializeField]
+    float StartSpeed = 0;
+    [SerializeField]
+    float AddSpeed = 0;
+    Rigidbody rb;
+
+    public AnimationCurve SpeedCurve;
+    float time = 0;
+
+    public float MaxDistnace = -1;
+    public float MinSpeed = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.AddForce(transform.forward * StartSpeed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.AddForce(transform.forward * AddSpeed * SpeedCurve.Evaluate(time));
+        Debug.Log(SpeedCurve.Evaluate(time));
+        time += Time.deltaTime;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!((TargetMask.value & 1 << other.gameObject.layer) > 0)) return;
+        Destroy(this.gameObject, DestroyAfterCollision);
+        rb.isKinematic = true;
+    }
+}
